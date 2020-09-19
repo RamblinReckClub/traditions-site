355dba9ab08951f7181bf49660e5fbdec90cce07
Keshav <pittkesh99@gmail.com>
2020-09-19 14:22:37 -0400

    Made it so there is a seamless transition between images in the opening screen slideshoe

--- Sports/sportsHomePage.js
+++ Sports/sportsHomePage.js
@@ -2,13 +2,20 @@ var imageIndex = -1;
 
 function loopThroughImages() {
     var imageSlides = document.getElementsByClassName("imagemantle")
+    var backgroundSlides = document.getElementsByClassName("backimagemantle")
     for (var i=0; i < imageSlides.length; i++) {
         imageSlides[i].style.display = "none";
+        backgroundSlides[i].style.display = "none";
     }
     imageIndex++;
     if (imageIndex >= imageSlides.length) {
         imageIndex = 0;
     }
     imageSlides[imageIndex].style.display = "block";
+    if (imageIndex >= imageSlides.length - 1) {
+        backgroundSlides[0].style.display = "block";
+    } else {
+        backgroundSlides[imageIndex + 1].style.display = "block";
+    }
     setTimeout(loopThroughImages, 5000); //the number is in milliseconds: 1000->1 second
 }
