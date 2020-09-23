8d849237274f1066b0df1db6a4d770cda02a1570
Keshav <pittkesh99@gmail.com>
2020-09-22 23:41:59 -0400

    Reworking side navigation bar

--- Sports/sportsHomePage.js
+++ Sports/sportsHomePage.js
@@ -1,5 +1,8 @@
 var imageIndex = -1;
 var open = false;
+var open_sports = false;
+var open_trads = false;
+var open_camp = false;
 
 function loopThroughImages() {
     var imageSlides = document.getElementsByClassName("imagemantle")
@@ -23,20 +26,62 @@ function loopThroughImages() {
 
 function opencloseSideBar() {
     if (open == false) {
-        document.getElementById("sidenavelement").style.width = "12%";
+        document.getElementById("sidenavelement").style.width = "20%";
+        document.getElementById("dimscreen").style.display = "block";
         open = true;
     } else if (open == true) {
+        document.getElementById("dimscreen").style.display = "none";
         document.getElementById("sidenavelement").style.width = "0%";
         open = false;
     }
 }
 
-function openSports() {
-    document.getElementById("sidenavsports").style.width = "10%";
-    document.getElementById("sidenavsports").style.borderLeft = "3px solid black";
+function expand_sports() {
+    if (open_sports == false) {
+        elements = document.getElementsByClassName("hidden_sidebar_sports");
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.setProperty("display", "block", "important");
+        }
+        open_sports = true;
+    } else if (open_sports == true) {
+        elements = document.getElementsByClassName("hidden_sidebar_sports");
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.setProperty("display", "none", "important");
+        }
+        open_sports = false;
+    }
 }
 
-function closeSports() {
-    document.getElementById("sidenavsports").style.width = "0%";
-    document.getElementById("sidenavsports").style.borderLeft = "0px solid black";
+function expand_traditions() {
+    if (open_trads == false) {
+        elements = document.getElementsByClassName("hidden_sidebar_trad");
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.setProperty("display", "block", "important");
+        }
+        open_trads = true;
+    } else if (open_trads == true) {
+        elements = document.getElementsByClassName("hidden_sidebar_trad");
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.setProperty("display", "none", "important");
+        }
+        open_trads = false;
+    }
 }
+
+
+function expand_campus_life() {
+    if (open_camp == false) {
+        elements = document.getElementsByClassName("hidden_sidebar_camp");
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.setProperty("display", "block", "important");
+        }
+        open_camp = true;
+    } else if (open_camp == true) {
+        elements = document.getElementsByClassName("hidden_sidebar_camp");
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.setProperty("display", "none", "important");
+        }
+        open_camp = false;
+    }
+}
+
