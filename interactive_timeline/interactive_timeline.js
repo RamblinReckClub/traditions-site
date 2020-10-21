be731be5461b82f837e5278b002f4aba57a16635
Keshav <pittkesh99@gmail.com>
2020-10-20 21:47:03 -0400

    Changed the code so only relevant years appear

--- interactive_timeline/interactive_timeline.js
+++ interactive_timeline/interactive_timeline.js
@@ -4,6 +4,21 @@ Element.prototype.addAfter = function (element) {
 
 var expanded_index = 0; 
 var open_logo = false;
+var list_1880s = [5, 6, 8];
+var list_1890s = [2, 3, 4, 6];
+var list_1900s = [4, 5, 6, 8];
+var list_1910s = [1, 3, 5, 6, 7];
+var list_1920s = [2, 9];
+var list_1930s = [3, 8];
+var list_1940s = [4, 5, 8];
+var list_1950s = [2, 5, 6, 7];
+var list_1960s = [1, 4, 7, 9];
+var list_1970s = [2, 3, 4, 8];
+var list_1980s = [1, 7, 9];
+var list_1990s = [1, 4, 6];
+var list_2000s = [2, 3, 4, 7, 8];
+var list_2010s = [1, 3, 9];
+var list_of_lists = [list_1880s, list_1890s, list_1900s, list_1910s, list_1920s, list_1930s, list_1940s, list_1950s, list_1960s, list_1970s, list_1980s, list_1990s, list_2000s, list_2010s]
 
 function open_years(year, index){
     if(expanded_index != index) {
@@ -12,11 +27,12 @@ function open_years(year, index){
         }
         var years_list = document.getElementById('years');
         var list_item = years_list.childNodes[index];
-        for (var i = 0; i < 9; i++){
+        var correct_list = list_of_lists[(index-1)/2];
+        for (var i=correct_list.length-1; i>=0; i--){
             list_element = document.createElement('li');
             list_element.className = index + '_dates'
             link_element = document.createElement('a');
-            var year_last_digit = 9-i;
+            var year_last_digit = correct_list[i];
             var year_text = year.substring(0, 3) + (year_last_digit.toString());
             link_element.appendChild(document.createTextNode(year_text));
             (function (year_text) {
