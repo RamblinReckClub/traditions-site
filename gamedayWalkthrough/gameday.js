e4dc70a46ad6db03ae53c36afc8aeae604a58132
Keshav <pittkesh99@gmail.com>
2020-10-25 10:29:20 -0400

    Fixed weird loading glitch

--- gamedayWalkthrough/gameday.js
+++ gamedayWalkthrough/gameday.js
@@ -13,17 +13,22 @@ function loadRightPageElements() {
     }
     shownSlideID = "slide" + sessionStorage.getItem("shownSlide");
     shownTextID = "text" + sessionStorage.getItem("shownText");
-    if (sessionStorage.getItem("shownSlide") != 1) {
-        var shownSlideElement = document.getElementById(shownSlideID);
-        var hiddenSlideElement = document.getElementById("slide1");
-        var shownTextElement = document.getElementById(shownTextID);
-        var hiddenTextElement = document.getElementById("text1");
-        hiddenSlideElement.style.display = "none";
-        shownSlideElement.style.display = "inline";
-        hiddenTextElement.style.display = "none";
-        shownTextElement.style.display = "grid";
-    } else {
-        sessionStorage.setItem("shownSlide", 1);
-        sessionStorage.setItem("shownText", 1);
-    }
+    var shownSlideElement = document.getElementById(shownSlideID);
+    var shownTextElement = document.getElementById(shownTextID);
+    shownSlideElement.style.display = "inline";
+    shownTextElement.style.display = "grid";
+    //if (sessionStorage.getItem("shownSlide") != 1) {
+    //    console.log("Test");
+    //    var shownSlideElement = document.getElementById(shownSlideID);
+    //    var hiddenSlideElement = document.getElementById("slide1");
+    //    var shownTextElement = document.getElementById(shownTextID);
+    //    var hiddenTextElement = document.getElementById("text1");
+    //    hiddenSlideElement.style.display = "none";
+    //    shownSlideElement.style.display = "inline";
+    //    hiddenTextElement.style.display = "none";
+    //    shownTextElement.style.display = "grid";
+    //} else {
+    //    sessionStorage.setItem("shownSlide", 1);
+    //    sessionStorage.setItem("shownText", 1);
+    //}
 }
